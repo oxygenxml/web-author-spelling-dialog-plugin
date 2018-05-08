b88abcc42066a2df20c6378310f86b0d64d71e96
ctalau <cristitalau@gmail.com>
2018-05-09 00:20:11 +0300

    Make spellcker wrap around

--- src/main/java/com/oxygenxml/webapp/plugins/spellcheck/GoToNextSpellingErrorOperation.java
+++ src/main/java/com/oxygenxml/webapp/plugins/spellcheck/GoToNextSpellingErrorOperation.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.swing.text.BadLocationException;
 
+import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import com.google.common.collect.ImmutableMap;
@@ -31,6 +32,11 @@ import ro.sync.exml.workspace.api.util.TextChunkDescriptor;
 @WebappRestSafe
 public class GoToNextSpellingErrorOperation extends AuthorOperationWithResult {
   /**
+   * Logger.
+   */
+  Logger logger = Logger.getLogger(GoToNextSpellingErrorOperation.class);
+  
+  /**
    * Attribute name for the start position of the last spelling error.
    */
   private static final String START_POS_ATTR_NAME = "com.oxygenxml.plugins.spellcheck.startPos";
@@ -47,6 +53,9 @@ public class GoToNextSpellingErrorOperation extends AuthorOperationWithResult {
     WebappSpellchecker spellchecker = docModel.getSpellchecker();
     
     SpellCheckingProblemInfo nextProblem = findNextProblem(docModel, fromCaret);
+    if (nextProblem == null) {
+      return null;
+    }
     
     saveCurrentProblem(docModel, nextProblem);
     
@@ -56,13 +65,31 @@ public class GoToNextSpellingErrorOperation extends AuthorOperationWithResult {
     
     String[] suggestions = findSuggestions(spellchecker, nextProblem);
     
-    ObjectMapper objectMapper = new ObjectMapper();
     try {
+      ObjectMapper objectMapper = new ObjectMapper();
       return objectMapper.writeValueAsString(ImmutableMap.of(
           "word", nextProblem.getWord(),
           "suggestions", suggestions));
     } catch (IOException e) {
       throw new AuthorOperationException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Return the offset from which we should start the spellchecking.
+   * 
+   * @param docModel the document model.
+   * @param fromCaret <code>true</code> if we should start from the carer pos.
+   * 
+   * @return The start offset.
+   */
+  private int getStartOffset(AuthorDocumentModel docModel, boolean fromCaret) {
+    if (fromCaret) {
+      return docModel.getSelectionModel().getCaretOffset();
+    } else {
+      EditingSessionContext editingContext = docModel.getAuthorAccess().getEditorAccess().getEditingContext();
+      Integer lastEnd = (Integer) editingContext.getAttribute(END_POS_ATTR_NAME);
+      return lastEnd != null ? lastEnd : 0;
     }
   }
 
@@ -79,32 +106,94 @@ public class GoToNextSpellingErrorOperation extends AuthorOperationWithResult {
   private SpellCheckingProblemInfo findNextProblem(AuthorDocumentModel docModel, boolean fromCaret)
       throws AuthorOperationException {
     WebappSpellchecker spellchecker = docModel.getSpellchecker();
-    int caretOffset = docModel.getSelectionModel().getCaretOffset();
     AuthorDocument document = docModel.getAuthorDocumentController().getAuthorDocumentNode();
-    List<TextChunkDescriptor> textDescriptors;
     
-    //TODO: start from the caret position or from last position.
-    if (fromCaret) {
-      textDescriptors = spellchecker.getTextDescriptors(caretOffset, document.getEndOffset());
-    } else {
-      textDescriptors = spellchecker.getTextDescriptors(document.getStartOffset(), document.getEndOffset());
+    int startOffset = getStartOffset(docModel, fromCaret);
+    
+    SpellCheckingProblemInfo problemInfo = 
+        runSpellcheck(spellchecker, startOffset, document.getEndOffset(), document.getEndOffset());
+    if (problemInfo == null) {
+      problemInfo = runSpellcheck(spellchecker, 0, startOffset, document.getEndOffset());
+    }
+    return problemInfo;
+  }
+
+  /**
+   * Run spellcheck between two offsets.
+   * 
+   * @param spellchecker The spellchecker.
+   * @param startOffset The start offset.
+   * @param endOffset The end offset.
+   * @param docLength The length of the document.
+   * 
+   * @return The first problem found, or null if everything is ok.
+   * 
+   * @throws AuthorOperationException
+   */
+  private SpellCheckingProblemInfo runSpellcheck(WebappSpellchecker spellchecker,
+      int startOffset, int endOffset, int docLength) throws AuthorOperationException {
+    logger.debug("Checking between " + startOffset + " " + endOffset);
+    int interval = 1000;
+    
+    int currentOffset = startOffset;
+    while (currentOffset < endOffset) {
+      int intervalEnd = Math.min(currentOffset + interval, endOffset);
+      SpellCheckingProblemInfo nextProblem = 
+          runSpellcheckSingleInterval(spellchecker, currentOffset, intervalEnd, docLength);
+      if (nextProblem != null) {
+        logger.debug("Found: " + nextProblem.getWord());
+        return nextProblem;
+      }
+      currentOffset = Math.min(currentOffset + interval, endOffset);
     }
     
-    SpellCheckingProblemInfo nextProblem = null;
+    return null;
+  }
+
+  /**
+   * Runs spellcheck on a single interval.
+   * 
+   * @param spellchecker The spellchecker.
+   * @param start The start offset of the interval.
+   * @param end The end offset of the interval.
+   * @param docLength The length of the document.
+   * 
+   * @return The first problem found or null if no problems were found.
+   * 
+   * @throws AuthorOperationException
+   */
+  private SpellCheckingProblemInfo runSpellcheckSingleInterval(WebappSpellchecker spellchecker,
+      int start, int end, int docLength) throws AuthorOperationException {
+    logger.debug("Checking interval between " + start + " " + end);
+
+    // Run spellcheck on a slightly larger interval and ignore problems
+    // at the boundaries - they may be caused by truncated words.
+    int boundary = 50;
+    List<TextChunkDescriptor> textDescriptors = 
+        spellchecker.getTextDescriptors(
+            Math.max(0, start - boundary), 
+            Math.min(docLength, end + boundary));
+    
     for (TextChunkDescriptor textDescriptor : textDescriptors) {
       try {
         List<SpellCheckingProblemInfo> problems = 
             spellchecker.check(Collections.singletonList(textDescriptor));
-        
-        if (problems != null && !problems.isEmpty()) {
-          nextProblem = problems.get(0);
-          break;
+        if (problems == null) {
+          continue;
+        }
+        for (SpellCheckingProblemInfo problem : problems) {
+          if (problem.getStartOffset() >= start && problem.getEndOffset() <= end) {
+            return problem;
+          } else {
+            // We might just found a truncated word at the end or start of the interval. 
+            // It will be found again in the next overlapping interval.  
+          }
         }
       } catch (IOException e) {
         throw new AuthorOperationException(e.getMessage(), e);
       }
     }
-    return nextProblem;
+    return null;
   }
 
   /**
@@ -144,7 +233,7 @@ public class GoToNextSpellingErrorOperation extends AuthorOperationWithResult {
     String[] suggestions = new String[0];
     try {
       SpellSuggestionsInfo suggestionInfo = 
-          spellchecker.getSuggestionsForWordAtPosition(nextProblem.getStartOffset());
+          spellchecker.getSuggestionsForWordAtPosition(nextProblem.getStartOffset() + 1);
       suggestions = suggestionInfo.getSuggestions();
     } catch (Exception e) {
       throw new AuthorOperationException(e.getMessage(), e);
